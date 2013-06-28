9b9bd7584fea3f07963447074a67637072e5784e
Miranda Mehta <jplisamaria@gmail.com>
2013-06-27 20:08:43 -0400

    First Twillio.

--- local_settings.py
+++ local_settings.py
@@ -5,8 +5,8 @@ Includes keys for Twilio, etc.  Second stanza intended for Heroku deployment.
 '''
 
 # Uncommet to configure in file.
-ACCOUNT_SID = "AC802d88dd13f2e3f8d8bc3f9b95eb9f06" 
-AUTH_TOKEN = "a610e5b81d025523706a2bbc381bf5ac"
+ACCOUNT_SID = "ACcc80917eb41fda681de0bba6f7926546" 
+AUTH_TOKEN = "0f7071c8917508f174ad31b36af0df41"
 BSSSPAM_APP_SID = "AP69a2d4c9cdceee480dfa9e9230c586c5"
 BSS_SPAM_ID = "+17032913341"
 
